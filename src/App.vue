8b18c451e7282f456904daab195309eb8e33ea2c
gmldnjs26 <imleesky265@gmail.com>
2021-05-24 22:39:07 +0900

    First

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div class="flex h-screen mx-72">
+      <div class="w-1/4 bg-white-200 pl-5">
+        <i class="fab fa-twitter text-2xl text-blue-600 mt-2 mb-6"></i>
+        <div class="flex flex-col text-lg items-start">
+          <div
+            class="hover:text-primary hover:bg-lighter px-2 py-2 rounded-full"
+          >
+            <i class="fas fa-home"></i>
+            <span class="ml-5 text-lg">Home</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-lighter px-2 py-2 rounded-full"
+          >
+            <i class="fas fa-home"></i>
+            <span class="ml-5 text-lg">Explore</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-lighter px-2 py-2 rounded-full"
+          >
+            <i class="fas fa-home"></i>
+            <span class="ml-5 text-lg">Notifications</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-lighter px-2 py-2 rounded-full"
+          >
+            <i class="fas fa-home"></i>
+            <span class="ml-5 text-lg">Messages</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-lighter px-2 py-2 rounded-full"
+          >
+            <i class="fas fa-home"></i>
+            <span class="ml-5 text-lg">Bookmarks</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-lighter px-2 py-2 rounded-full"
+          >
+            <i class="fas fa-home"></i>
+            <span class="ml-5 text-lg">Lists</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-lighter px-2 py-2 rounded-full"
+          >
+            <i class="fas fa-home"></i>
+            <span class="ml-5 text-lg">Profile</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-lighter px-2 py-2 rounded-full"
+          >
+            <i class="fas fa-home"></i>
+            <span class="ml-5 text-lg">More</span>
+          </div>
+        </div>
+      </div>
+      <div class="w-2/4 bg-green-200"></div>
+      <div class="w-1/4 bg-blue-200"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
