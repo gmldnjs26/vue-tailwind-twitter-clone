bc25e9484a57f176501e9d687f1e0eb9a4b1bcb9
gmldnjs26 <imleesky265@gmail.com>
2021-07-25 13:15:53 +0900

    firebase

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import App from './App.vue'
 import './index.css'
 import router from './router'
 
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
