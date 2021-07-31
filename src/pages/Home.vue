edf9dca8f10e18257783862876d2206e65ef3d8f
gmldnjs26 <imleesky265@gmail.com>
2021-07-31 21:20:24 +0900

    add tweeting

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -9,11 +9,14 @@
         <img src="https://picsum.photos/100" class="w-10 h-10 rounded-full" />
         <div class="flex flex-col w-full">
           <textarea
+            v-model="tweetContents"
             class="w-full text-lg font-bold focus:outline-none ml-3 mb-3 resize-none"
             placeholder="What's happening"
           />
           <div class="text-right">
-            <button class="text-white bg-primary rounded-full font-bold hover:bg-dark w-20 h-10">트윗</button>
+            <button @click="tweeting" class="text-white bg-primary rounded-full font-bold hover:bg-dark w-20 h-10">
+              트윗
+            </button>
           </div>
         </div>
       </div>
@@ -30,11 +33,42 @@
 <script>
 import Trends from '../components/Trends.vue'
 import Tweet from '../components/Tweet.vue'
+import { computed, ref } from 'vue'
+import { store } from '../store'
+import { TWEET_COLLECTION } from '../firebase'
 
 export default {
   components: {
     Trends,
     Tweet,
+  },
+  setup() {
+    const tweetContents = ref('')
+    const curUser = computed(() => store.state.user)
+
+    const tweeting = async () => {
+      try {
+        const doc = TWEET_COLLECTION.doc()
+        await doc.set({
+          id: doc.id,
+          tweet_contents: tweetContents.value,
+          uid: curUser.value.uid,
+          created_at: Date.now(),
+          num_comments: 0,
+          num_retweets: 0,
+          num_likes: 0,
+        })
+        tweetContents.value = ''
+      } catch (e) {
+        console.log('tweeting error', e)
+      }
+    }
+
+    return {
+      tweetContents,
+      tweeting,
+      curUser,
+    }
   },
 }
 </script>
