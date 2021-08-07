f45d5617a168259161dd58e56d10760875b9f94e
gmldnjs26 <imleesky265@gmail.com>
2021-08-07 21:22:57 +0900

    modify api gettweet

--- src/api/getTweetInfo.ts
+++ src/api/getTweetInfo.ts
@@ -1,17 +1,31 @@
-import { USER_COLLECTION, RETWEET_COLLECTION } from '../firebase'
+import { LIKE_COLLECTION, RETWEET_COLLECTION, USER_COLLECTION } from '../firebase'
 
-const getTweetInfo = async (tweets, curUser) => {
-  await tweets.value.map(async (tweet) => {
-    const doc = await USER_COLLECTION.doc(tweet.uid).get()
-    const userInfo = {
-      profile_image_url: doc.data().profile_image_url,
-      email: doc.data().email,
-      username: doc.data().username,
-    }
+export default async (tweet, currentUser) => {
+  // Adding user info
+  const doc = await USER_COLLECTION.doc(tweet.uid).get()
+  tweet.profile_image_url = doc.data().profile_image_url
+  tweet.email = doc.data().email
+  tweet.username = doc.data().username
 
-    const snapshot = await RETWEET_COLLECTION.where('from_tweet_id', '==', tweet.id)
-      .where('uid', '==', curUser.uid)
-      .get()
-    return { ...tweet, ...userInfo, isRetweeted: snapshot.empty ? false : true }
-  })
+  // Adding retweet info
+  const retweetSnapshot = await RETWEET_COLLECTION.where('from_tweet_id', '==', tweet.id)
+    .where('uid', '==', currentUser.uid)
+    .get()
+  if (retweetSnapshot.empty) {
+    tweet.isRetweeted = false
+  } else {
+    tweet.isRetweeted = true
+  }
+
+  // Adding likes info
+  const likeSnapshot = await LIKE_COLLECTION.where('from_tweet_id', '==', tweet.id)
+    .where('uid', '==', currentUser.uid)
+    .get()
+  if (likeSnapshot.empty) {
+    tweet.isLiked = false
+  } else {
+    tweet.isLiked = true
+  }
+
+  return tweet
 }
